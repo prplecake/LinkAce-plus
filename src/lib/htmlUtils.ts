efaef8f4b2164a600529780a8ff4d9ef1847a1c3
prplecake <me@prplecake.com>
2023-06-15 13:20:55 -0500

    Add logging to htmlUtils.show and .hide

--- src/lib/htmlUtils.ts
+++ src/lib/htmlUtils.ts
@@ -1,12 +1,18 @@
+import {Logger} from './logger';
+
+const logger = new Logger('htmlUtils');
+
 export const byId = (id: string) => {
   return document.getElementById(id);
 };
 
 export const hide = (elem: HTMLElement | null) => {
+  logger.log('hiding element: ', elem);
   if (elem) elem.style.display = 'none';
 };
 
 export const show = (elem: HTMLElement | null) => {
+  logger.log('showing element: ', elem);
   if (elem) elem.style.display = 'unset';
 };
 
