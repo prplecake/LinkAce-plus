4ad2e5c7ff2c58ea59b26354e03b56ce348e3c8a
Matty Jorgensen <me@prplecake.com>
2023-12-03 18:58:22 -0600

    Fix quotes

--- tests/lib/html.test.ts
+++ tests/lib/html.test.ts
@@ -1,16 +1,16 @@
-import {byId} from '../../src/functions/html';
+import {byId} from "../../src/functions/html";
 
-describe('testing byId', () => {
-  test('success return is not null', () => {
+describe("testing byId", () => {
+  test("success return is not null", () => {
     document.body.innerHTML = `
       <div id="root"></div>    
     `;
-    expect(byId('root')).toBeTruthy();
+    expect(byId("root")).toBeTruthy();
   });
-  test('failure return is undefined', () => {
+  test("failure return is undefined", () => {
     document.body.innerHTML = `
       <div id="root"></div>    
     `;
-    expect(byId('rootbeer')).toBeNull();
+    expect(byId("rootbeer")).toBeNull();
   });
 });
