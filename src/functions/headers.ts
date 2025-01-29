979037703f6e0a6c3562afb3bd2e463504174295
Matthew Jorgensen <matthew.jorgensen@evidentiq.com>
2025-01-28 18:56:57 -0600

    Add Content-Type as default header

--- src/functions/headers.ts
+++ src/functions/headers.ts
@@ -1,4 +1,4 @@
-export const getDefaultHeaders = (userInfo?: any, token?: string) => {
+export const getDefaultHeaders = (userInfo?: UserInfo, token?: string) => {
   let authHeader = "";
   if (userInfo) authHeader = `Bearer ${userInfo.authToken}`;
   if (token) authHeader = `Bearer ${token}`;
@@ -6,5 +6,6 @@ export const getDefaultHeaders = (userInfo?: any, token?: string) => {
   return {
     Authorization: authHeader,
     Accept: "application/json",
+    "Content-Type": "application/json",
   };
 };
